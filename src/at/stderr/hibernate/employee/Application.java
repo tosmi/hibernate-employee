6ef8a51a7a1053ea2f6dc3b00e810879b042240a
Toni Schmidbauer <toni@stderr.at>
2021-10-09 22:37:56 +0200

    work in homework

--- src/at/stderr/hibernate/employee/Application.java
+++ src/at/stderr/hibernate/employee/Application.java
@@ -17,11 +17,30 @@ public class Application {
 
         Session session = factory.getCurrentSession();
         Employee employee = new Employee("Toni", "Schmidbauer", "Red Hat");
+        System.out.println("Employee ID: " + employee.getId());
         session.beginTransaction();
         session.save(employee);
         session.getTransaction().commit();
+        System.out.println("Employee ID: " + employee.getId());
+        System.out.println("Employee: " + employee.hashCode());
 
+        session = factory.getCurrentSession();
+        session.beginTransaction();
+        Employee byIdEmployee = session.get(Employee.class, employee.getId());
+        System.out.println("Employee ID: " + byIdEmployee.getId());
+        System.out.println("Employee: " + byIdEmployee.hashCode());
+        session.getTransaction().commit();
+
+        session = factory.getCurrentSession();
+        session.beginTransaction();
+        session.delete(byIdEmployee);
+        System.out.println("Session contains employee: " + session.contains(byIdEmployee));
+        session.getTransaction().commit();
+
+        session = factory.getCurrentSession();
+        session.beginTransaction();
         List<Employee> employeeList=  session.createQuery("from Employee").getResultList();
+        session.getTransaction().commit();
 
         for(Employee e : employeeList) {
             System.out.println(e);
